c471e3bab1142f8ae6bfe66b432ba451778db21f
DalaliLama <vidyarupak@gmail.com>
2021-06-13 06:20:43 +0000

    added navigation using alan

---
+++ Navigation.js
@@ -0,0 +1,16 @@
+intent(`(Navigate|Take|Go|Show me| to| app|application) usage`, p => {
+        p.play({command: 'navigation', route: 'appstats'});
+        p.play(`Navigating to Application Usage Stats`);
+});
+intent(`(Navigate|Take|Go|Show me| to|) FAQ`, p => {
+        p.play({command: 'navigation', route: 'faq'});
+        p.play(`Navigating to FAQ`);
+});
+intent(`(Navigate|Take|Go|Show me| to|) home`, p => {
+        p.play({command: 'navigation', route: 'home'});
+        p.play(`Navigating back to home`);
+});
+intent(`(Take|Go me|) back`, p => {
+        p.play({command: 'navigation', route: 'back'});
+        p.play(`Going back`);
+});
